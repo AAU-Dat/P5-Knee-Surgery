0508c90149bf1b233480d113d18bb724b4735e60
Houi <hougaardandreas@gmail.com>
2022-09-15 14:19:02 +0200

    transposer

---
+++ data-transposer.py
@@ -0,0 +1,23 @@
+import csv
+import numpy as np
+import math
+
+data = np.zeros((377982, 20))
+
+with open("./temp_knee_data2" + ".csv") as csvfile:
+    print(csvfile.name + " is opened.")
+    csvreader = csv.reader(csvfile)
+
+    #populate matrix with temp_knee_data
+    count = 0
+    for row in csvreader:
+        data[count] = row;
+        count = count + 1
+
+final_array = np.zeros((16434, 284))
+
+for i in range(0, 377982, 23):
+    final_array[math.ceil(i/23)][0:8] = data[i][0:8]
+    for x in range(0, 23):
+        final_array[math.ceil(i/23)][8+12*x:20+12*x] = data[i][8:20]
+print("hi")
