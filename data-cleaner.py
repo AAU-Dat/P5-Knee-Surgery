b60d5d7b93a1a840b97115e9ed2e477a9ae52f2f
Niklas Egebak Joensen <joensenniklas@gmail.com>
2022-09-15 10:22:02 +0200

    cleaned metadata as step 1.5

--- data-cleaner.py
+++ data-cleaner.py
@@ -8,7 +8,7 @@ import numpy as np
 data = np.zeros((6561328, 31))
 
 ### Step 1 - Loads every dataset into a big data set ###
-for x in range(7):
+for x in range(8):
     with open("./data/data" + str(x+1) + ".csv") as csvfile:
 
         print(csvfile.name)
@@ -22,9 +22,12 @@ for x in range(7):
             count = count + 1
             #print(row[0])
 
+### step 1.5 - remove the last 11 metadata rows
+data = np.delete(data, slice(20, 31, 1), 1)
+
 ### Step 2 - Removes any redundent rows that don't have 23 rows ###
 previous = data[0][0]
-array_of_parameters = np.zeros((23, 31))
+array_of_parameters = np.zeros((23, 20))
 index = 1
 temp_row = data[0]
 
@@ -36,13 +39,17 @@ for row in data:
     if row[0] == previous:
         index += 1
         if index == 23:
-            print("Added knee data to array")
+            #print("Added knee data to array")
             array_of_parameters[0] = temp_row
             np.savetxt(temp_knee_data, array_of_parameters, newline="\n", delimiter=",", fmt="%13f")
             index = 1
     else:
-        array_of_parameters = np.zeros((23, 31))
+        array_of_parameters = np.zeros((23, 20))
         index = 1
         temp_row = row
     # This row we are looking at, is stored in the previous row
     previous = row[0]
+
+### Step 3: transpose data
+
+### Step 4: remove duplicates (with numpy.unique likely)
