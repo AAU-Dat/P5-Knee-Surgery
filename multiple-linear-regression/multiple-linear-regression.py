2cdb74c288e603f3de957a5804134e97c199bf42
pimber <christofferctj@gmail.com>
2022-09-27 23:31:15 +0200

    The multiple linear regression has been finished, and it gives out the correct result. The only thing now is to check for the min and max value of r

--- multiple-linear-regression/multiple-linear-regression.py
+++ multiple-linear-regression/multiple-linear-regression.py
@@ -1,10 +1,19 @@
+import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy as sp
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
+matplotlib.use('Agg')
 
+# Constants to change style in graph
+MarkerSize = 0.1
+DotColor = 'Blue'
 
+# This function make sure that y has all the 276 columns
 def gives_x_all_param_header():
     x = []
     for i in range(1, 24):
@@ -17,20 +26,20 @@ def gives_x_all_param_header():
 # importing data
 df = pd.read_csv('../data_processing/final_final_final.csv')
 
-# print(df)
-
-
 x = df[gives_x_all_param_header()]
 y = df[['ACL_k']]
 
+# This prints out the x and y, it's just for checking that everything is correct
 # print(x)
 # print(y)
 
+# Make a for loop that iterates thourgh x numbers of models of x_test and ----------------------------------------------
+# saves the best and worse r value and the average r value
 regr = linear_model.LinearRegression()
 regr.fit(x, y)
 
 # creating train and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.6)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2) # Remember to shuffle test
 
 # creating a regression model
 model = LinearRegression()
@@ -39,8 +48,33 @@ model = LinearRegression()
 model.fit(x_train, y_train)
 
 # making predictions
-predictions = model.predict(x_test)
+predictions_test = model.predict(x_test)
+predictions_train = model.predict(x_train)
 
-# model evaluation
-print('mean_squared_error : ', mean_squared_error(y_test, predictions))
-print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+# model evaluation for predict_test
+print('Predict_test')
+print('Root Mean Squared Error (RMSE) : ', mean_squared_error(y_test, predictions_test, squared=False))
+print('r2 value is: ', r2_score(y_test, predictions_test))
+print()
+# print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+
+# model evaluation fro predict_train
+print('Predict_train')
+print('Root Mean Squared Error (RMSE) : ', mean_squared_error(y_test, predictions_test, squared=False))
+print('r2 value is: ', r2_score(y_test, predictions_test))
+# print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+# ----------------------------------------------------------------------------------------------------------------------
+
+# prints out the graph for predictions_test
+plt.scatter(y_test, predictions_test, color=DotColor, s=MarkerSize)
+# plt.plot(y_test, predictions, color="blue", linewidth=3)
+
+# To save the graph for predictions_test
+plt.savefig('./figures/prediction_test.png')
+
+# prints out the graph for predictions_train
+plt.figure() # This makes a new figure
+plt.scatter(y_train, predictions_train, color=DotColor, s=MarkerSize)
+
+# To save the graph for predictions_train
+plt.savefig('./figures/prediction_train.png')
