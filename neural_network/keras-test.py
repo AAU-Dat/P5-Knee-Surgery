a6001a202a2da135de1bdf4cb8c7d92839e32d62
Jamie Hammer <jamie-hammer@hotmail.com>
2022-10-08 16:04:31 +0200

    Update keras-test.py

--- neural_network/keras-test.py
+++ neural_network/keras-test.py
@@ -1,12 +1,13 @@
+import keras.callbacks
 from keras import layers
 from keras import metrics
-import keras_tuner
+import keras_tuner as kt
 import pandas as pd
 import time
-
 from keras.models import Sequential
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+from keras.regularizers import L2
 
 LOG_DIR = f"{int(time.time())}"
 
@@ -26,40 +27,66 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 
 def build_model(hp):
+    """
+    Build model for hyperparameters tuning
+
+    hp: HyperParameters class instance
+    """
+
+    # Hyperparameters
+    number_of_layers = hp.Int("n_layers", 2, 6)
+    learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+    units = [hp.Int(name='layer_1_units', min_value=16, max_value=256, step=16),
+             hp.Int(name='layer_2_units', min_value=16, max_value=256, step=16),
+             hp.Int(name='layer_3_units', min_value=16, max_value=256, step=16),
+             hp.Int(name='layer_4_units', min_value=16, max_value=256, step=16),
+             hp.Int(name='layer_5_units', min_value=16, max_value=256, step=16),
+             hp.Int(name='layer_6_units', min_value=16, max_value=256, step=16)]
+
+    # Parameters
+    input_shape = X_train.shape[1:]
+    regularizer = L2(0.001)
+
     model = Sequential([
-        layers.Flatten(name='Input-Layer', input_shape=X_train.shape[1:])
+        layers.Flatten(name='input_layer', input_shape=input_shape)
     ])
 
     # Hidden Layers
-    for i in range(hp.Int("n_layers", 1, 5)):
+    for i in range(number_of_layers):
         model.add(layers.Dense(
-            hp.Int('units', min_value=32, max_value=512, step=32),
-            name='Hidden-Layer-' + str(i + 1),
-            activation='relu')
+            units=units[i],
+            name='hidden_layer_' + str(i + 1),
+            activation='relu',
+            kernel_regularizer=regularizer)
         )
+        # TODO : reduce over fitting the model with -> model.add(layers.Dropout(0.05))
 
     # Output Layer
-    model.add(layers.Dense(1, activation='softmax', name='Output-Layer'))
+    model.add(layers.Dense(1, activation='softmax', name='output_layer'))
 
-    learning_rate = hp.Float("Learning-Rate", min_value=1e-4, max_value=1e-2, sampling="log")
-
-    model.compile(optimizer=Adam(learning_rate=learning_rate), loss='mean_squared_error', metrics=[metrics.RootMeanSquaredError()])
+    model.compile(optimizer=Adam(learning_rate=learning_rate),
+                  loss=[metrics.mean_squared_error,
+                        metrics.mean_absolute_error],
+                  metrics=[metrics.RootMeanSquaredError(),
+                           metrics.MeanAbsoluteError()],)
     return model
 
 
-tuner = keras_tuner.RandomSearch(
+tuner = kt.RandomSearch(
     build_model,
-    objective=keras_tuner.Objective("val_root_mean_squared_error", direction='min'),
+    objective=kt.Objective("val_root_mean_squared_error", direction='min'),
     max_trials=10,
     executions_per_trial=2,
     directory=LOG_DIR,
-    project_name='p5_neural_network'
+    project_name='P5-Knee-Surgery',
+    seed=40
 )
 
-tuner.search(X_train, y_train, epochs=1, batch_size=64, validation_data=(X_test, y_test))
+stop_early = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+tuner.search(X_train, y_train, epochs=2, batch_size=64, validation_data=(X_test, y_test), callbacks=[stop_early])
 best_model = tuner.get_best_models()[0]
 
-best_model.build(input_shape=X_train.shape[1:])
+best_model.build()
 
 print(tuner.results_summary())
 print(best_model.summary())
