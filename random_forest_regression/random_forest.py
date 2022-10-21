4a4aebec34361d7efaa3cb347395fcad01e283ce
Niklas Egebak Joensen <joensenniklas@gmail.com>
2022-10-21 11:46:36 +0200

    Created skeleton for random forest regression.

---
+++ random_forest_regression/random_forest.py
@@ -0,0 +1,32 @@
+from sklearn.ensemble import RandomForestRegressor as RFR
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+def gives_x_all_param_header():
+    x = []
+    for i in range(1, 24):
+        x.extend(['trans_x_' + str(i), 'trans_y_' + str(i), 'trans_z_' + str(i), 'rot_z_' + str(i),
+                  'rot_x_' + str(i), 'rot_y_' + str(i), 'F_x_' + str(i), 'F_y_' + str(i), 'F_z_' + str(i),
+                  'M_x_' + str(i), 'M_y_' + str(i), 'M_z_' + str(i)])
+    return x
+
+df = pd.read_csv('../data_processing/final_final_final.csv')
+ligament_headers = ['ACL_k', 'ACL_epsr', 'PCL_k', 'PCL_epsr', 'MCL_k', 'MCL_epsr', 'LCL_k', 'LCL_epsr']
+x = df[gives_x_all_param_header()]
+y = df[ligament_headers[0]]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=90, test_size=10, shuffle=True)
+regressor = RFR(n_estimators=10, random_state=0)
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+
+print(r2)
+print(mae)
+print(mse)
