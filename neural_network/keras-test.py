936c48b1ad7faffd18bf72fb77b78713dec43ec0
Jamie Hammer <jamie-hammer@hotmail.com>
2022-10-24 18:04:56 +0200

    Update keras-test.py
    Made the dropout rate dynamic by converting it to a hyperparameter.
    
    Refactored some code that was unnecessarily long

--- neural_network/keras-test.py
+++ neural_network/keras-test.py
@@ -2,16 +2,18 @@ import keras.callbacks
 from keras import layers
 from sklearn import metrics
 import pandas as pd
-import time
 from keras.models import Sequential
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from keras.regularizers import L2
-from keras_tuner import RandomSearch, BayesianOptimization, Hyperband
+from keras_tuner import BayesianOptimization
 from keras.losses import MeanSquaredLogarithmicError
-msle = MeanSquaredLogarithmicError()
+from datetime import datetime
 
-LOG_DIR = f"{int(time.time())}"
+msle = MeanSquaredLogarithmicError()
+now = datetime.now()
+
+LOG_DIR = 'Test ran on ' + now.strftime("%d-%b-%Y at %H:%M")
 
 # Import Dataset and prepare for Training
 df = pd.read_csv('../data_processing/final_final_final.csv', index_col=0)
@@ -36,66 +38,44 @@ def build_model(hp):
     hp: HyperParameters class instance
     """
 
-    _input_shape = X_train.shape[1:]
-    _regularizer = L2(0.001)
-    _loss = msle
-    _metrics = [msle]
-
     hp_number_of_layers = hp.Int('n_layers', min_value=2, max_value=8)
     hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
 
     # Create Sequential Model and setup Input Layer
-    model = Sequential([layers.Flatten(name='input_layer', input_shape=_input_shape)])
+    model = Sequential([layers.Flatten(name='input_layer', input_shape=X_train.shape[1:])])
 
     # Hidden Layers
     for i in range(hp_number_of_layers):
         number = str(i + 1)
-        layer_name = 'hidden_layer_' + number
-        units_name = 'units_layer_' + number
 
-        hp_units = hp.Int(name=units_name, min_value=32, max_value=512, step=32)
+        hp_units = hp.Int(name='units_layer_' + number, min_value=32, max_value=512, step=32)
 
-        model.add(layers.Dense(units=hp_units,
-                               name=layer_name,
-                               activation='relu',
-                               kernel_regularizer=_regularizer))
+        model.add(layers.Dense(units=hp_units, name='hidden_layer_' + number, activation='relu',
+                               kernel_regularizer=L2(0.001)))
 
-        model.add(layers.Dropout(0.05))
+        model.add(layers.Dropout(rate=hp.Float(name='dropout_' + number,
+                                               min_value=0.0, max_value=0.5, default=0.25, step=0.05)))
 
     # Output Layer
     model.add(layers.Dense(1, activation='linear', kernel_initializer='normal', name='output_layer'))
 
     # Setup Compiler
-    model.compile(optimizer=Adam(learning_rate=hp_learning_rate),
-                  loss=_loss,
-                  metrics=_metrics)
+    model.compile(optimizer=Adam(learning_rate=hp_learning_rate), loss=msle, metrics=[msle])
     return model
 
 
 tuner = BayesianOptimization(
-    build_model,
-    objective='val_mean_squared_logarithmic_error',
-    max_trials=10,
-    # executions_per_trial=3,
-    directory=LOG_DIR,
-    project_name='P5-Knee-Surgery',
-    seed=40,
+    build_model, objective='val_mean_squared_logarithmic_error', max_trials=10,
+    num_initial_points=50, directory=LOG_DIR, project_name='P5-Knee-Surgery', seed=40
 )
 
 stop_early = keras.callbacks.EarlyStopping(monitor='val_mean_squared_logarithmic_error', patience=3)
 tuner.search(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test), callbacks=[stop_early])
-best_model = tuner.get_best_models()[0]
 
+best_model = tuner.get_best_models()[0]
 best_model.build()
 
-print("The best model has now been build!")
-
-best_model.fit(
-    X_train,
-    y_train.ravel(),
-    epochs=10,
-    batch_size=64
-)
+best_model.fit(X_train, y_train.ravel(), epochs=10, batch_size=64)
 
 expected_y = y_test
 predicted_y = best_model.predict(X_test)
@@ -106,6 +86,4 @@ current_msle = metrics.mean_squared_log_error(expected_y, predicted_y)
 print("R2 Score: ", current_r2_score)
 print("MSLE Score: ", current_msle)
 
-# mean squared logarithmic error
-# maybe print this?
-# msle(y_test, best_model.predict(X_test)).numpy()
+print(best_model.summary())
