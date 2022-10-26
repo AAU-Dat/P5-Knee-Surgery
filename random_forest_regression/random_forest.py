c0fff1da3395a13666f17d994e952f6c84defaa7
Niklas Egebak Joensen <joensenniklas@gmail.com>
2022-10-26 11:27:30 +0200

    fixed random_forest_all_parameters() to take a singular target vector at a time

--- random_forest_regression/random_forest.py
+++ random_forest_regression/random_forest.py
@@ -3,11 +3,11 @@ from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 
 #Program skal bygges under keras for at vi kan bruge tensorflow. Se jamie branch.
 #import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+#print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 df = pd.read_csv('../data_processing/final_final_final.csv')
 ligament_headers = ['ACL_k', 'ACL_epsr', 'PCL_k', 'PCL_epsr', 'MCL_k', 'MCL_epsr', 'LCL_k', 'LCL_epsr']
@@ -29,15 +29,21 @@ def write_results_to_file(r_2, mae, mse, estimators, max_features, ligament):
 def print_status(estimators, max_features, ligament):
     print(f'Finished max_features= {max_features}, estimators= {estimators} ligament={ligament}')
 
+#
+# retain best entries for best values:
+#
+#def evaluate_best_config(estimators, max_features, ligament, r_2, mae, mse):
+
+
 
 def random_forest_all_parameters(estimators, ligaments):
     x = df[gives_x_all_param_header()]
-    y = df[ligament_headers]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=True)
 
-    for i in range(1, estimators+1):
-        for j in range(1, 12):
-                for l in range(ligaments):
+    for l in range(ligaments):
+        y = df[ligament_headers[l]]
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=True)
+        for i in range(1, estimators+1):
+            for j in range(1, 12):
                     regressor = RFR(n_estimators=i, max_features=0.45+(j*0.05))
                     regressor.fit(x_train, y_train)
                     y_pred = regressor.predict(x_test)
@@ -45,12 +51,12 @@ def random_forest_all_parameters(estimators, ligaments):
                     mae = mean_absolute_error(y_test, y_pred)
                     mse = mean_squared_error(y_test, y_pred)
 
-                    write_results_to_file(r_2=r2, mae=mae, mse=mse, estimators=i, max_features=j, ligament=ligament_headers[l])
+                    write_results_to_file(r_2=r2, mae=mae, mse=mse, estimators=i, max_features=0.45+(j*0.05), ligament=ligament_headers[l])
                     print_status(max_features=0.45+(j*0.05), estimators=i, ligament=ligament_headers[l])
 
 
 
-random_forest_all_parameters(10, 1)
+random_forest_all_parameters(1, 2)
 
 
 
