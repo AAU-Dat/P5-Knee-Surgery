305e7d5b7753e31f97c97b5ec71f5aec387bd6a2
Jamie Hammer <jamie-hammer@hotmail.com>
2022-11-01 14:44:38 +0100

    Update keras-test.py
    - Segregated the hyperparameter search space from the model building.
    - Added Root Mean Squared Error evaluation on the best model found.
    - Created regions with descriptions to make the code more readable.
    - Some minor refactors.

--- neural_network/keras-test.py
+++ neural_network/keras-test.py
@@ -1,7 +1,9 @@
 import keras.callbacks
+import math
+import pandas as pd
+import keras_tuner as kt
 from keras import layers
 from sklearn import metrics
-import pandas as pd
 from keras.models import Sequential
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
@@ -15,59 +17,70 @@ now = datetime.now()
 
 LOG_DIR = 'Test ran on ' + now.strftime("%d-%b-%Y at %H:%M")
 
-# Import Dataset and prepare for Training
+# <editor-fold desc="Preparing the Data Set">
+
 df = pd.read_csv('../data_processing/final_final_final.csv', index_col=0)
-df.describe().transpose()
 
 result_columns = ['ACL_k', 'ACL_epsr', 'PCL_k', 'PCL_epsr', 'MCL_k', 'MCL_epsr', 'LCL_k', 'LCL_epsr']
 target_column = ['ACL_k']
 predictors = list(set(list(df.columns)) - set(result_columns))
 df[predictors] = df[predictors] / df[predictors].max()
-df.describe().transpose()
 
 X = df[predictors].values
 y = df[target_column].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=40)
 
+# </editor-fold>
+
+# <editor-fold desc="Configure the search space">
+
+HP = kt.HyperParameters()
+
+HP.Int('n_layers', min_value=2, max_value=8)
+HP.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+for i in range(8):
+    HP.Int(name='units_layer_' + str(i + 1), min_value=32, max_value=512, step=32)
+    HP.Float(name='dropout_' + str(i + 1), min_value=0.0, max_value=0.5, default=0.25, step=0.05)
+
+# </editor-fold>
+
+# <editor-fold desc="Building the model for hyperparameters tuning">
+
 
 def build_model(hp):
-    """
-    Build model for hyperparameters tuning
-
-    hp: HyperParameters class instance
-    """
-
-    hp_number_of_layers = hp.Int('n_layers', min_value=2, max_value=8)
-    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
-
     # Create Sequential Model and setup Input Layer
     model = Sequential([layers.Flatten(name='input_layer', input_shape=X_train.shape[1:])])
 
     # Hidden Layers
-    for i in range(hp_number_of_layers):
-        number = str(i + 1)
+    for j in range(hp.get('n_layers')):
+        number = str(j + 1)
 
-        hp_units = hp.Int(name='units_layer_' + number, min_value=32, max_value=512, step=32)
+        model.add(layers.Dense(units=hp.get('units_layer_' + number), name='hidden_layer_' + number,
+                               activation='relu', kernel_regularizer=L2(0.001)))
 
-        model.add(layers.Dense(units=hp_units, name='hidden_layer_' + number, activation='relu',
-                               kernel_regularizer=L2(0.001)))
-
-        model.add(layers.Dropout(rate=hp.Float(name='dropout_' + number,
-                                               min_value=0.0, max_value=0.5, default=0.25, step=0.05)))
+        model.add(layers.Dropout(rate=hp.get('dropout_' + number)))
 
     # Output Layer
     model.add(layers.Dense(1, activation='linear', kernel_initializer='normal', name='output_layer'))
 
     # Setup Compiler
-    model.compile(optimizer=Adam(learning_rate=hp_learning_rate), loss=msle, metrics=[msle])
+    model.compile(optimizer=Adam(learning_rate=hp.get('learning_rate')), loss=msle, metrics=[msle])
     return model
+
+# </editor-fold>
+
+# <editor-fold desc="Optimising the model">
 
 
 tuner = BayesianOptimization(
-    build_model, objective='val_mean_squared_logarithmic_error', max_trials=10,
+    build_model, objective='val_mean_squared_logarithmic_error', max_trials=10, hyperparameters=HP,
     num_initial_points=50, directory=LOG_DIR, project_name='P5-Knee-Surgery', seed=40
 )
+
+# </editor-fold>
+
+# <editor-fold desc="Finding the best model">
 
 stop_early = keras.callbacks.EarlyStopping(monitor='val_mean_squared_logarithmic_error', patience=3)
 tuner.search(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test), callbacks=[stop_early])
@@ -75,15 +88,28 @@ tuner.search(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test
 best_model = tuner.get_best_models()[0]
 best_model.build()
 
+print(best_model.summary())
+
+# </editor-fold>
+
+# <editor-fold desc="Training the model">
+
 best_model.fit(X_train, y_train.ravel(), epochs=10, batch_size=64)
+
+# </editor-fold>
+
+# <editor-fold desc="Evaluating the model">
 
 expected_y = y_test
 predicted_y = best_model.predict(X_test)
 
 current_r2_score = metrics.r2_score(expected_y, predicted_y)
+current_mse = metrics.mean_squared_error(expected_y, predicted_y)
 current_msle = metrics.mean_squared_log_error(expected_y, predicted_y)
+current_rmse = math.sqrt(current_mse)
 
-print("R2 Score: ", current_r2_score)
-print("MSLE Score: ", current_msle)
+print("The coefficient of determination (R^2): ", current_r2_score)
+print("Mean Squared Logarithmic Error: ", current_msle)
+print("Root Mean Squared Error: ", current_rmse)
 
-print(best_model.summary())
+# </editor-fold>
