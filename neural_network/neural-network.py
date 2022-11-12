5d9ce08f2fe91d5575c4ee1860abff2afa710768
Jamie Hammer <jamie-hammer@hotmail.com>
2022-11-12 04:04:22 +0100

    Cross-Validation
    - I have implemented cross-validation
        - I have also made a script for you to check it out and play around with it
    - Minor refactors and bug fixes
    - Added some comments

--- neural_network/neural-network.py
+++ neural_network/neural-network.py
@@ -19,8 +19,10 @@ LOG_DIR = f"Test ran on {now}"
 MODEL_DIR = f"{LOG_DIR}/models/"
 
 # Data Set preparation
+ACL_k, ACL_epsr, PCL_k, PCL_epsr = 'ACL_k', 'ACL_epsr', 'PCL_k', 'PCL_epsr'
+MCL_k, MCL_epsr, LCL_k, LCL_epsr = 'MCL_k', 'MCL_epsr', 'LCL_k', 'LCL_epsr'
 df = pd.read_csv('../data_processing/final_final_final.csv', index_col=0)
-result_columns = ['ACL_k', 'ACL_epsr', 'PCL_k', 'PCL_epsr', 'MCL_k', 'MCL_epsr', 'LCL_k', 'LCL_epsr']
+result_columns = [ACL_k, ACL_epsr, PCL_k, PCL_epsr, MCL_k, MCL_epsr, LCL_k, LCL_epsr]
 input_shape = (276,)                            # input is a row of entries (284 - [8 result columns])
 seed = 69                                       # to get reproducible data splits and hyperparameters
 train_ratio = 0.80                              # percentage of the data set allocated to train and tune
@@ -47,13 +49,13 @@ def build_model(hp):
 
     # Hidden Layers
     for j in range(hp.get('number_of_layers')):
-        number = str(j + 1)
+        number = f"{j + 1}"
 
-        model.add(layers.Dense(units=hp.get('units_layer_' + number), name='hidden_layer_' + number,
+        model.add(layers.Dense(units=hp.get(f"units_layer_{number}"), name=f"hidden_layer_{number}",
                                activation='relu', kernel_regularizer=L2(0.001)))
 
         if hp.get('use_dropout'):
-            model.add(layers.Dropout(rate=hp.get('dropout_' + number)))
+            model.add(layers.Dropout(rate=hp.get(f"dropout_{number}")))
 
     # Output Layer
     model.add(layers.Dense(1, activation='linear', kernel_initializer='normal', name='output_layer'))
@@ -67,7 +69,7 @@ def build_model(hp):
 
 # <editor-fold desc="Building the Hyperparameter tuner">
 
-def build_tuner(x_train, y_train, x_test, y_test):
+def build_tuner(x_train, y_train, x_val, y_val):
     # Create the Hyperband tuner with the objective to minimise the error on the validation data
     tuner = Hyperband(
         build_model, objective='val_mean_absolute_error', max_epochs=20, hyperparameters=HP,
@@ -78,9 +80,8 @@ def build_tuner(x_train, y_train, x_test, y_test):
     stop_early = keras.callbacks.EarlyStopping(monitor='val_mean_absolute_error', patience=3)
 
     # Search for the best model and its hyperparameters
-    tuner.search(x_train, y_train, epochs=50, batch_size=64,
-                 validation_data=(x_test, y_test), callbacks=[stop_early],
-                 shuffle=True, use_multiprocessing=True, workers=8)
+    tuner.search(x_train, y_train, epochs=50, validation_data=(x_val, y_val),
+                 callbacks=[stop_early], use_multiprocessing=True, workers=8)
 
     return tuner
 
@@ -89,14 +90,14 @@ def build_tuner(x_train, y_train, x_test, y_test):
 
 # <editor-fold desc="Training the best Model">
 
-def train_hypermodel(target, tuner, x_train, y_train):
+def train_hypermodel(target, tuner, x_train, y_train, x_val, y_val):
     best_hp = tuner.get_best_hyperparameters()[0]
 
-    # Build model with the best hyperparameters and train it (with 20% of the train data) for 50 epochs
+    # Build model with the best hyperparameters and train it for 50 epochs
     best_model = tuner.hypermodel.build(best_hp)
-    history = best_model.fit(x_train, y_train.ravel(), epochs=50, validation_split=0.20, batch_size=64)
+    history = best_model.fit(x_train, y_train.ravel(), epochs=50, validation_data=(x_val, y_val))
 
-    # Find the best epoch
+    # Find the best epoch from the first training
     val_mae_per_epoch = history.history['val_mean_absolute_error']
     best_epoch = val_mae_per_epoch.index(min(val_mae_per_epoch)) + 1
 
@@ -104,7 +105,7 @@ def train_hypermodel(target, tuner, x_train, y_train):
     hypermodel = tuner.hypermodel.build(best_hp)
 
     # Retrain the model with the best found epoch
-    hypermodel.fit(x_train, y_train.ravel(), epochs=best_epoch, validation_split=0.20, batch_size=64)
+    hypermodel.fit(x_train, y_train.ravel(), epochs=best_epoch, validation_data=(x_val, y_val))
 
     # Save the model to the log directory for future reference
     hypermodel.save(f"{MODEL_DIR}{target}")
@@ -125,6 +126,7 @@ def evaluate_model(x_test, y_test, hypermodel, target):
     current_mae = metrics.mean_absolute_error(expected_y, predicted_y)
     current_rmse = math.sqrt(current_mse)
 
+    # TODO : Return strings or similar, so that the evaluation can be seen for all of the eight models at once
     print(f"This is the evaluation of the {target}")
     print(f"The coefficient of determination (R^2): {current_r2_score}")
     print(f"Mean Absolute Error (MAE): {current_mae}")
@@ -133,8 +135,10 @@ def evaluate_model(x_test, y_test, hypermodel, target):
 # </editor-fold>
 
 
-def manage_model(target):
-    # Set up the input
+# <editor-fold desc="Handling a Model">
+
+def handle_model(target):
+    # Set up and normalise the input
     predictors = list(set(list(df.columns)) - set(result_columns))
     df[predictors] = df[predictors] / df[predictors].max()
 
@@ -142,25 +146,31 @@ def manage_model(target):
     x = df[predictors].values
     y = df[target].values
 
-    # Split the data set (80% train, 20% test) for cross-validation using the seed
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=seed)
+    # Train gets the train_ratio of the data set
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1 - train_ratio)
 
-    # Get the best model and hyperparameters from the tuner
-    tuner = build_tuner(x_train, y_train, x_test, y_test)
+    # Both Validation and Test get 50% each of the remainder
+    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test,
+                                                    test_size=test_ratio / (test_ratio + validation_ratio))
 
-    # Get the best model and hyperparameters from the tuner
-    best_model = train_hypermodel(target, tuner, x_train, y_train)
+    # Build a Tuner and search for Hyperparameters on the Train data, test on the Validate data
+    tuner = build_tuner(x_train, y_train, x_val, y_val)
 
-    # Evaluate the model
+    # Build and train the best Model on the Train data, test on the Validate data
+    best_model = train_hypermodel(target, tuner, x_train, y_train, x_val, y_val)
+
+    # Evaluate the model using cross-validation on the Test data
     evaluate_model(x_test, y_test, best_model, target)
 
+# </editor-fold>
 
-manage_model(['ACL_k'])
+
+handle_model(ACL_k)
 # TODO : Run for the other models too; but try to utilise the GPU
-# manage_model(['ACL_espr'])
-# manage_model(['PCL_k'])
-# manage_model(['PCL_espr'])
-# manage_model(['MCL_k'])
-# manage_model(['MCL_espr'])
-# manage_model(['LCL_k'])
-# manage_model(['LCL_espr'])
+# handle_model(['ACL_epsr'])
+# handle_model(['PCL_k'])
+# handle_model(['PCL_epsr'])
+# handle_model(['MCL_k'])
+# handle_model(['MCL_epsr'])
+# handle_model(['LCL_k'])
+# handle_model(['LCL_epsr'])
