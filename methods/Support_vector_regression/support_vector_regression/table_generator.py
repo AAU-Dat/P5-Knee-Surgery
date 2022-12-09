e0cb84c57464ee7185a83c5b9fc15d0eb419e2d7
Houi <hougaardandreas@gmail.com>
2022-12-10 00:00:45 +0100

    table gen has been made easier to use

--- methods/Support_vector_regression/support_vector_regression/table_generator.py
+++ methods/Support_vector_regression/support_vector_regression/table_generator.py
@@ -10,25 +10,53 @@ def gives_caption_header_array():
     return ['ACL\_k', 'ACL\_epsr', 'PCL\_k', 'PCL\_epsr', 'MCL\_k', 'MCL\_epsr', 'LCL\_k', 'LCL\_epsr']
 
 
+def left_side_columns():
+    columns = 'l'
+    for i in range(len(hyperparameters_renaming)):
+        columns += 'l'
+    return columns
 
-def table_gen(target, caption_target):
+
+def change_df_column_names(dataframe):
+    for i in range(len(hyperparameters_renaming)):
+        dataframe.rename(columns={hyperparameters_renaming[i][0]: hyperparameters_renaming[i][1]}, inplace=True)
+
+
+def table_gen(target):
     df = pd.read_csv(f'models\\{target}\\{target}_GridsearchCV_Results.csv')
-    df.rename(columns={'param_svc__C': 'C'}, inplace=True)
-    df.rename(columns={'mean_test_score': 'mean test score'}, inplace=True)
-    table = df[["C", "mean test score"]]
-    table.style.set_properties(**{'text-align': 'left'})
-    str = table.style.hide(axis="index").to_latex(hrules=True,
-                                                  caption=f"results from SVR gridsearch on {caption_target}",
-                                                  label="table:svr_grid_results",
-                                                  position_float="centering")
-    file = open(f"C:\\Users\\joens\\source\\repos\\p5\\P5-Knee-Surgery\\methods\\Support_vector_regression\\svr_table_{target}.csv", 'a')
+    change_df_column_names(df)
+    df.rename(columns={'mean_test_score': 'rmse'}, inplace=True)
+    table = df[["C", "rmse"]]
+    str = table.style.hide(axis="index").to_latex(column_format=left_side_columns(),
+                                                  hrules=True,
+                                                  caption=f"results from {method_name} {search_method_name} on {caption_headers[target]}",
+                                                  label=f"table:{method_name}_{search_method_name}_results_{target}")
+    file = open(f"{method_name}_table_{target}.csv", 'w')
     file.write(str)
     file.close()
     print()
 
 
 headers = gives_header_array()
-caption_headers = gives_caption_header_array()
+caption_headers = {'ACL_k': 'ACL\_k',
+         'ACL_epsr': 'ACL\_epsr',
+         'PCL_k': 'PCL\_k',
+         'PCL_epsr': 'PCL\_epsr',
+         'MCL_k': 'MCL\_k',
+         'MCL_epsr': 'MCL\_epsr',
+         'LCL_k': 'LCL\_k',
+         'LCL_epsr': 'LCL\_epsr'}
+
+# fit these variables to your method
+method_name = 'SVR'
+search_method_name = 'gridsearch'
+hyperparameters_renaming = [['param_svc__C', 'C']]
+
+
+
+
+
+
 for i in range(8):
-    table_gen(headers[i], caption_headers[i])
+    table_gen(headers[i])
 
